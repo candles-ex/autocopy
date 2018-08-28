a7b1978d3f9dfbed7605ca5cabd1a80d721513bb
Mizukoshi Keita <mzks@km.phys.sci.osaka-u.ac.jp>
2018-08-28 16:34:08 +0900

    For ssh version

--- auto.py
+++ auto.py
@@ -7,7 +7,7 @@ import glob
 import numpy as np
 from time import sleep
 import shutil
-
+import subprocess
 
 Run = 10
 MaxRunNum = 10 #1000
@@ -16,24 +16,25 @@ sizeMatrix = np.zeros((MaxRunNum-1,MaxRunNum-1))
 
 while True:
     copyReady = False  #check existance of next run file
-    
+    toFileList = subprocess.check_output(["ssh","mzks@lxmzks","ls","-la","to"]).decode('utf-8')   
     for sRun in reversed(range(1,MaxRunNum)):
         for ssRun in reversed(range(1,MaxRunNum)):
             filename = 'Run' + str(Run).zfill(3) + '-' + str(sRun).zfill(3) + '-' + str(ssRun).zfill(3) + '.dat'
             fromfile = './from/' + filename
-            tofile = './to/' + filename
+            tofile = 'mzks@lxmzks:~/to/' + filename
 
             if os.path.exists(fromfile) == True:
                 size = os.path.getsize(fromfile)
                 if sizeMatrix[sRun-1][ssRun-1] == size:
                     if copyReady == True:
-                        if os.path.exists(tofile) == False:
+                        if toFileList.find(filename) == -1:
                             print("copy", filename)
-                            shutil.copy2(fromfile, tofile)
+                            #shutil.copy2(fromfile, tofile)
+                            subprocess.call(["scp",fromfile, tofile])
                     else:
                         copyReady = True
                 else:
                     sizeMatrix[sRun-1][ssRun-1] = size
 
-    sleep(600)
+    sleep(10)
 

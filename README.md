474c5283c62c94164cd2bd00ad0acbf673e71b57
Mizukoshi Keita <mzks@km.phys.sci.osaka-u.ac.jp>
2018-08-28 20:25:52 +0900

    send ped. and sgl. runs

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 
-# CANDLES Data translation tool
+# CANDLES Data transportation tool
+Author  : K. Mizukoshi
+Date    : 2018 Aug. 28
 
-- watch run number and translate data
+## Overview
+This script is to transport CANDLES raw data on time.
+The auto.py watch a data directory, transport the data that satisfies the following conditions.
+
+- No update in 10 minutes.
+- The next run has been started.
+- The file has not been created in the directory transported.
+
+## Usage
+```
+>nohup python auto.py &
+```
+Then, a shifter doesn't have to do an additional work.
+
+## Monitoring / troubleshoot
+```
+>ps ux | grep auto
+
+candles 17112   0.0  0.2  4385384  15264 s007  SN    8:05PM   0:00.87 python auto.py
+```
+If you cannot find the process, see nohup.out.
+
+You can
+
+#. restart this script
+#. transport the data using autocopy.sh 
+#. report me (K. Mizukoshi mzks@ne.phys.sci.osaka-u.ac.jp) and exparts.
+
--- auto.py
+++ auto.py
@@ -3,25 +3,30 @@
 
 import sys
 import os
-import glob
 import numpy as np
 from time import sleep
-import shutil
+import datetime
 import subprocess
 
 Run = 10
-MaxRunNum = 10 #1000
-
+MaxRunNum = 1000
 sizeMatrix = np.zeros((MaxRunNum-1,MaxRunNum-1))
+
+toDir = 'mzks@lxmzks:~/to/'
+fromDir = './from/'
 
 while True:
     copyReady = False  #check existance of next run file
-    toFileList = subprocess.check_output(["ssh","mzks@lxmzks","ls","-la","to"]).decode('utf-8')   
+    toFileList = subprocess.check_output(["ssh","mzks@lxmzks","ls","-la","to"]).decode('utf-8') #get remote file list
+
     for sRun in reversed(range(1,MaxRunNum)):
         for ssRun in reversed(range(1,MaxRunNum)):
             filename = 'Run' + str(Run).zfill(3) + '-' + str(sRun).zfill(3) + '-' + str(ssRun).zfill(3) + '.dat'
-            fromfile = './from/' + filename
-            tofile = 'mzks@lxmzks:~/to/' + filename
+            fromfile = fromDir + filename
+            tofile = toDir + filename
+
+            filenameSgl = 'Sgl' + str(Run).zfill(3) + '-' + str(sRun).zfill(3) + '-001.dat'
+            filenamePed = 'Ped' + str(Run).zfill(3) + '-' + str(sRun).zfill(3) + '-001.dat'
 
             if os.path.exists(fromfile) == True:
                 size = os.path.getsize(fromfile)
@@ -29,12 +34,16 @@ while True:
                     if copyReady == True:
                         if toFileList.find(filename) == -1:
                             print("copy", filename)
-                            #shutil.copy2(fromfile, tofile)
                             subprocess.call(["scp",fromfile, tofile])
+                            if ssRun == 1:
+                                print("copy sgl and ped", filename)
+                                subprocess.call(["scp",fromDir+filenameSgl, toDir])
+                                subprocess.call(["scp",fromDir+filenamePed, toDir])
                     else:
                         copyReady = True
                 else:
                     sizeMatrix[sRun-1][ssRun-1] = size
 
+    print('loop end ',datetime.datetime.now())
     sleep(10)
 
